a82e40237eb18bbd70d600937614e2c270f1e58a
1449584369@qq.com <1449584369@qq.com>
2023-10-17 19:13:25 +0800

    增加微信通知

--- alipan_auto_check_in.sh
+++ alipan_auto_check_in.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #需要每月更新一次
 refresh_token=$1
+#pushplus_token
+pushplus_token=$2
 ##获取access_token
 access_token_command="curl --location --request POST 'https://auth.aliyundrive.com/v2/account/token' --header 'Content-Type: application/json' --data '{
     \"grant_type\": \"refresh_token\",
@@ -8,7 +10,15 @@ access_token_command="curl --location --request POST 'https://auth.aliyundrive.c
 }' | jq '.access_token' | sed 's/\"//g'";
 access_token=`eval $access_token_command`
 if [ "$access_token" = "null" ];
-  then echo "refresh_token失效,请更换"
+  then 
+    pushplus_command="curl --location --request POST 'http://www.pushplus.plus/send/' \
+--header 'Content-Type: application/json' \
+--data '{
+    \"token\":\"$pushplus_token\",
+    \"title\":\"阿里云盘自动签到\",
+    \"content\":\"refresh_token失效,请及时更换\"
+}'"
+    eval $pushplus_command;
 else
 sleep 2
 ##自动签到
@@ -23,6 +33,13 @@ reward_command="curl --location --request POST 'https://member.aliyundrive.com/v
 --data '{
     \"signInDay\": \"$sign_in_count\"
 }'"
-notice=`eval $reward_command | jq '.result.notice'`
-echo "签到成功，你已经签到$sign_in_count次，本次签到奖励————$notice"
+notice=`eval $reward_command | jq '.result.notice' | sed 's/\"//g'`
+pushplus_command="curl --location --request POST 'http://www.pushplus.plus/send/' \
+--header 'Content-Type: application/json' \
+--data '{
+    \"token\":\"$pushplus_token\",
+    \"title\":\"阿里云盘自动签到\",
+    \"content\":\"签到成功，你已经签到$sign_in_count次，本次签到奖励————$notice\"
+}'"
+eval $pushplus_command
 fi
